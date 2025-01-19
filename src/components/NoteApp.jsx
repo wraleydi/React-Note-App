c8c2c14c1fa260ade4bef2924162c3a8c64e3eeb
wraleydi <raleydi06@gmail.com>
2025-01-20 00:32:16 +0700

    buat input catatan baru

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -3,6 +3,7 @@ import NoteList from './notes/NoteList';
 import HeaderApp from './header/HeaderApp';
 import { getInitialData } from '../utils';
 import MessageNote from './notes/MessageNotes';
+import NoteInput from './create_notes/NoteInput';
 
 class NoteApp extends React.Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class NoteApp extends React.Component {
     this.onSearchHandler = this.onSearchHandler.bind(this);
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
     this.onArchiveHandler = this.onArchiveHandler.bind(this);
+    this.onAddNoteHandler = this.onAddNoteHandler.bind(this)
   }
 
   onDeleteHandler(id) {
@@ -37,6 +39,23 @@ class NoteApp extends React.Component {
     this.setState({ notes });
   }
 
+  onAddNoteHandler({ title, body, createdAt }) {
+    this.setState((prevState) => {
+      return {
+        notes: [
+          {
+            id: +new  Date(),
+            title,
+            body,
+            createdAt,
+            archived: false,
+          },
+          ...prevState.notes
+        ]
+      }
+    })
+  }
+
   render() {
     const searchNote = this.state.notes.filter((notes) =>
       notes.title.toLowerCase().includes(this.state.search.toLowerCase())
@@ -54,6 +73,7 @@ class NoteApp extends React.Component {
       <div>
         <HeaderApp onSearch={this.onSearchHandler} />
         <div className="note-app__body">
+          <NoteInput addNote={this.onAddNoteHandler} />
           <h2>Your Notes</h2>
           {activeNote.length > 0 ? (
             <NoteList
---
+++ src/components/create_notes/NoteInput.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+class NoteInput extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: '',
+            body: '',
+            createdAt: new Date().toISOString(),
+            maxTitleLimit: 50,
+        }
+
+        this.onTitleChangeHandler = this.onTitleChangeHandler.bind(this)
+        this.onBodyChangeHandler = this.onBodyChangeHandler.bind(this)
+        this.onSubmitHandler = this.onSubmitHandler.bind(this)
+    }
+
+    onTitleChangeHandler(event) {
+        this.setState(() => {
+            return {
+                title: event.target.value
+            }
+        })
+    }
+
+    onBodyChangeHandler(event) {
+        this.setState(() => {
+            return {
+                body: event.target.value
+            }
+        })
+    }
+
+    onSubmitHandler(event) {
+        event.preventDefault()
+        this.props.addNote(this.state)
+
+        this.setState({
+            title: '',
+            body: '',
+            createdAt: new Date().toISOString(),
+            maxTitleLimit: 50,
+        })
+    }
+
+    render() {
+
+        const limitTitle = this.state.maxTitleLimit - this.state.title.length
+
+        return (
+            <div className="note-input">
+                <h2>Create Your Note</h2>
+                <form action="" onSubmit={this.onSubmitHandler}>
+                    <p className="note-input__title__char-limit">sisa karakter: { limitTitle }</p>
+                    <input
+                    className="note-input__title" 
+                    type="text"
+                    placeholder="Masukkan Judul..."
+                    value={this.state.title}
+                    onChange={this.onTitleChangeHandler}
+                    required
+                    />
+                    <textarea 
+                    className="note-input__body"
+                    type="text"
+                    placeholder="Masukkan Isi..."
+                    value={this.state.body}
+                    onChange={this.onBodyChangeHandler}
+                    required
+                     />
+                     <button type="submit">Buat Catatan</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default NoteInput
